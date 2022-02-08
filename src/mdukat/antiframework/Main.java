4b72f6490769ce765b25bc2812c98b5ecb8ee679
d3suu <mateusz.dukat1@gmail.com>
2022-02-08 21:58:53 +0100

    More server settings (IP, backlog size)

--- src/mdukat/antiframework/Main.java
+++ src/mdukat/antiframework/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Server server = new Server();
-        server.start(8080, ExampleHandler.class);
+        server.start("127.0.0.1", 8080, ExampleHandler.class);
     }
 }
 
--- src/mdukat/antiframework/Server.java
+++ src/mdukat/antiframework/Server.java
@@ -1,5 +1,6 @@
 package mdukat.antiframework;
 
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,12 +15,56 @@ Calls: Worker
 public class Server {
 
     private ServerSocket serverSocket;
+    private int port;
+    private Class worker;
+    public static String version = "dev";
+    private static int defaultBacklog = 50;
 
+    // Start using default IP address
     public void start(int port, Class worker){
         try {
             serverSocket = new ServerSocket(port);
-            System.out.println("Started server at " + port);
-            while(true){
+            this.port = port;
+            this.worker = worker;
+            serve();
+        } catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    // Start using specified IP address
+    public void start(String IP, int port, Class worker){
+        try {
+            InetAddress ineta = InetAddress.getByName(IP);
+            serverSocket = new ServerSocket(port, defaultBacklog, ineta);
+            this.port = port;
+            this.worker = worker;
+            serve();
+        } catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    // Start using specified IP address and backlog value
+    public void start(String IP, int port, int backlog, Class worker){
+        try {
+            InetAddress ineta = InetAddress.getByName(IP);
+            serverSocket = new ServerSocket(port, backlog, ineta);
+            this.port = port;
+            this.worker = worker;
+            serve();
+        } catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+    private void serve(){
+        try {
+            System.out.println("Started antiframe server at " + port);
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
                 Worker newWorker = (Worker) worker.getDeclaredConstructor().newInstance();
                 newWorker.setClientSocket(clientSocket);
@@ -32,6 +77,10 @@ public class Server {
     }
 
     public static String getServerHeader(){
-        return "Server: antiframework/dev";
+        return "Server: antiframework/" + getServerVersion();
+    }
+
+    public static String getServerVersion(){
+        return version;
     }
 }
